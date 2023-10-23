86d90cd9ef708bf123b98f6153e96072c800c68c
유애나 <77397206+Teerapat28@users.noreply.github.com>
2023-10-23 18:40:43 +0700

    accordion Render Questions Toggle Question

---
+++ README.md
@@ -0,0 +1,4 @@
+## Figma URL
+
+[Accordion](https://www.figma.com/file/TAwJ3kWOqkw0o8UVtAMOHO/Accordion?node-id=0%3A1&t=1YEti8xBykw69tBH-1)
+
---
+++ src/Questions.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import SingleQuestion from "./SingleQuestion";
+
+const Questions = ({ questions, activeId, toggleQuestion }) => {
+  return (
+    <section className="container">
+      <h1>Questions</h1>
+      {questions.map((question) => {
+        return (
+          <SingleQuestion
+            key={question.id}
+            {...question}
+            activeId={activeId}
+            toggleQuestion={toggleQuestion}
+          />
+        );
+      })}
+    </section>
+  );
+};
+
+export default Questions;
